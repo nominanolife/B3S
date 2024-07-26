bcfbcb5a5ec3e4d4469aa17031e86e473063c8d3
Exylerr <148752815+Exylerr@users.noreply.github.com>
2024-07-26 12:57:27 +0800

    Revert "all goods"
    This reverts commit 3f68fc688ef5a451913a8cb566e5b3c2493ae279.

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 // Initialize AOS
 AOS.init();
 
-// Redirect function for login options
-function redirectToLogin(page) {
-    window.location.href = page;
-}
-
-// Show login modal on button click
-document.addEventListener('DOMContentLoaded', function() {
-    var loginButton = document.getElementById('loginButton');
-    loginButton.addEventListener('click', function() {
-        $('#loginModal').modal('show');
-    });
-});
-
 // Event listeners for buttons
 document.getElementById('enrollNowBtn').addEventListener('click', function() {
     this.classList.toggle('hover');
@@ -30,3 +17,15 @@ $('#videoModal').on('hidden.bs.modal', function () {
     video.currentTime = 0;
 });
 
+// Show login modal on button click
+document.addEventListener('DOMContentLoaded', function() {
+    var loginButton = document.getElementById('loginButton');
+    loginButton.addEventListener('click', function() {
+        $('#loginModal').modal('show');
+    });
+});
+
+// Redirect function for login options
+function redirectToLogin(page) {
+    window.location.href = page;
+}
