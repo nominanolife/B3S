b2e63bd373b51936541802cf00f0918db5bf05a1
Exylerr <148752815+Exylerr@users.noreply.github.com>
2024-08-09 16:48:30 +0800

    additional design changes

--- JS/admininstructorlist.js
+++ JS/admininstructorlist.js
@@ -11,3 +11,14 @@ document.addEventListener('DOMContentLoaded', function () {
         instructorModal.hide();
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.button-right');
+    
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            buttons.forEach(btn => btn.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+});
