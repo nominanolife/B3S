28d6533cc3c79e521e275152ed83d4e5304d3250
Luvlee00 <edmelynsybanez@iskolarngbayan.pup.edu.ph>
2024-10-28 13:46:17 +0800

    fucking toggle password

--- JS/reg.js
+++ JS/reg.js
@@ -1,34 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Set up password toggle functionality
-    function setupPasswordToggle(toggleId, passwordId) {
-        const togglePassword = document.getElementById(toggleId);
-        if (togglePassword) {
-            togglePassword.addEventListener('click', function () {
-                const passwordInput = document.getElementById(passwordId);
-                const icon = this.querySelector('i');
-                if (passwordInput.type === 'password') {
-                    passwordInput.type = 'text';
-                    icon.classList.remove('fa-eye-slash');
-                    icon.classList.add('fa-eye');
-                } else {
-                    passwordInput.type = 'password';
-                    icon.classList.remove('fa-eye');
-                    icon.classList.add('fa-eye-slash');
-                }
-            });
-        }
-    }
-    // Call setupPasswordToggle function
-    setupPasswordToggle('togglePassword', 'password');
-    // Function to handle login link
-    const loginLink = document.getElementById('loginLink');
-    if (loginLink) {
-        loginLink.addEventListener('click', function (event) {
-            event.preventDefault(); // Prevent the default link behavior
-            window.location.href = 'login.html'; // Redirect to the login page
-        });
-    }
-});
 
 $(document).ready(function() {
     // Show the modal when the page loads with the backdrop static option
